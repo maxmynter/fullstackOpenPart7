a636b5ddb6f718ae1d2086c96b56f14880dd266e
Max Mynter <maxmynter@me.com>
2023-01-07 22:28:02 +0100

    Ex. 7.4

--- src/components/SingleAnecdote.js
+++ src/components/SingleAnecdote.js
@@ -5,14 +5,20 @@ const SingleAnecdote = ({ anecdotes }) => {
   const anecdoteToDisplay = anecdotes.find((anecdote) => anecdote.id === id);
   return (
     <div>
-      <h2>
-        {anecdoteToDisplay.content} by {anecdoteToDisplay.author}
-      </h2>
-      <div>has {anecdoteToDisplay.votes} votes</div>
-      <div>
-        for more info see{" "}
-        <a href={anecdoteToDisplay.info}>{anecdoteToDisplay.info}</a>
-      </div>
+      {!anecdoteToDisplay ? (
+        <h3>404: Anecdote with id {id} does not exist</h3>
+      ) : (
+        <>
+          <h2>
+            {anecdoteToDisplay.content} by {anecdoteToDisplay.author}
+          </h2>
+          <div>has {anecdoteToDisplay.votes} votes</div>
+          <div>
+            for more info see{" "}
+            <a href={anecdoteToDisplay.info}>{anecdoteToDisplay.info}</a>
+          </div>
+        </>
+      )}
     </div>
   );
 };
