6a177866dd7175761734c877437795ad28f048d6
Max Mynter <maxmynter@me.com>
2023-01-31 10:14:14 +0100

    Ex. 7.15

--- bloglist/bloglist-frontend/src/components/usersInfo.js
+++ bloglist/bloglist-frontend/src/components/usersInfo.js
@@ -1,6 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getUsersInfo } from "../reducer/userReducer";
+
+const UserDetailView = ({ user }) => {
+  const [isClicked, setIsClicked] = useState(false);
+  const toggleDetails = () => {
+    setIsClicked(!isClicked);
+  };
+  return (
+    <>
+      {!isClicked ? (
+        <p key={"user_info_" + user.name} onClick={toggleDetails}>
+          {user.name} {"   "}
+          {user.blogs.length}
+        </p>
+      ) : (
+        <div onClick={toggleDetails}>
+          <h2>
+            {user.name}
+            {"'s Blogs"}
+          </h2>
+          <ul>
+            {user.blogs.map((blog) => (
+              <li key={"ul_element_blog_" + blog.title}>{blog.title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
 
 const UserView = () => {
   const dispatch = useDispatch();
@@ -10,19 +39,17 @@ const UserView = () => {
   }, []);
   return (
     <>
-      <h1>Users</h1>
       {users.length > 0 && (
-        <h4>
-          {"Name"} {"   "} {"# Blogs"}
-        </h4>
+        <>
+          <h1>Users</h1>
+          <h4>
+            {"Name"} {" || "} {"N Blogs"}
+          </h4>
+          {users.map((usr) => (
+            <UserDetailView user={usr} key={usr.name + "_detail_view"} />
+          ))}
+        </>
       )}
-      {users.length > 0 &&
-        users.map((usr) => (
-          <p key={"user_info_" + usr.name}>
-            {usr.name} {"   "}
-            {usr.blogs.length}
-          </p>
-        ))}
     </>
   );
 };
